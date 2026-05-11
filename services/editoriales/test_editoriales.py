fbef0b0e702974bad0800244f8fdb7cc96ff561f
“Juan <juanycamila582@gmail.com>
2026-05-11 10:29:01 -0500

    Upda3

--- services/editoriales/test_editoriales.py
+++ services/editoriales/test_editoriales.py
@@ -7,9 +7,11 @@ class Test_editoriales:
     def setup_class(self):
         self.url = "http://localhost:5083/editoriales"
         sql_pais = "INSERT IGNORE INTO paises (idPais, nombre, continente) VALUES ('CO', 'Colombia', 'America')"
+
         mi_cursor.execute(sql_pais)
         mi_db.commit()
         sql = "INSERT IGNORE INTO editoriales (idEditorial, nombre, idPais) VALUES ('ED01', 'Editorial Prueba', 'CO')"
+
         mi_cursor.execute(sql)
         mi_db.commit()
 
@@ -21,18 +23,18 @@ class Test_editoriales:
     def test_lista_editoriales(self):
         esperado = "editoriales"
         calculado = requests.get(self.url)
+
         assert calculado.status_code == 200
         assert calculado.json()["mensaje"] == esperado
 
     @pytest.mark.parametrize(
         ["nuevo_entrada", "esperado_entrada"],
-        [
-            ({"id": "1234", "nombre": "Nueva Editorial", "idPais": "CO"}, "Editorial agregada con éxito"),
-            ({"id": "4321", "nombre": "Editorial Prueba", "idPais": "CO"}, "Id de editorial ya existe"),
-        ]
+        [({"id": "1234", "nombre": "Nueva Editorial", "idPais": "CO"}, "Editorial agregada con éxito"),
+        ({"id": "4321", "nombre": "Editorial Prueba", "idPais": "CO"}, "Id de editorial ya existe"),]
     )
     def test_agregar(self, nuevo_entrada, esperado_entrada):
         calculado = requests.post(self.url, json=nuevo_entrada)
+
         assert calculado.status_code == 200
         assert esperado_entrada == calculado.json()["mensaje"]
 
@@ -42,6 +44,7 @@ class Test_editoriales:
     )
     def test_busqueda(self, id_entrada, esperado_entrada):
         calculado = requests.get(f"{self.url}/{id_entrada}")
+
         assert calculado.status_code == 200
         assert esperado_entrada in calculado.json()["mensaje"]
 
@@ -49,6 +52,7 @@ class Test_editoriales:
         id = "1234"
         nuevo = {"nombre": "Editorial Modificada", "idPais": "CO"}
         calculado = requests.put(f"{self.url}/{id}", json=nuevo)
+
         assert calculado.status_code == 200
         assert "Editorial modificada con éxito" in calculado.json()["mensaje"]
 
@@ -56,14 +60,16 @@ class Test_editoriales:
         id = "N0"
         nuevo = {"nombre": "Nadie", "idPais": "CO"}
         calculado = requests.put(f"{self.url}/{id}", json=nuevo)
+
         assert calculado.status_code == 200
         assert "Editorial no existe" in calculado.json()["mensaje"]
 
     @pytest.mark.parametrize(
         ["id_entrada", "esperado_entrada"],
-        [("1234", "Editorial eliminada con éxito!"), ("N0", "Editorial no existe")]
-    )
+        [("1234", "Editorial eliminada con éxito!"), ("N0", "Editoriales no existe")]
+)
     def test_elimina(self, id_entrada, esperado_entrada):
         calculado = requests.delete(f"{self.url}/{id_entrada}")
+
         assert calculado.status_code == 200
         assert esperado_entrada in calculado.json()["mensaje"]
